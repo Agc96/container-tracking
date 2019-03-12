9f945ec5315942ef8d12e72d8c21323282e359ef
Agc96 <anthony.gutierrez@pucp.pe>
2019-03-12 03:06:27 -0500

    Avances 2019-03-12

--- scraper/error.py
+++ scraper/error.py
@@ -1,18 +1,29 @@
 from datetime import datetime
 
 class TrackingScraperError(Exception):
-    """Custom exception for the Tracking Scraper class."""
-    def __init__(self, message = None, container = None, carrier = None, exception = None):
+    """
+    Custom exception for the Tracking Scraper class.
+    """
+    
+    def __init__(self, message, container = None, carrier = None, exception = None):
         # Call base class constructor
         super().__init__(message)
+        
         # Declare custom attributes
-        self.container = container
-        self.carrier = carrier
-        self.time = datetime.today()
+        self.__message = message
+        self.__container = container
+        self.__carrier = carrier
+        self.__datetime = datetime.today()
+        self.__exception = exception
+    
     def __str__(self):
-        return super().__str__()
+        # Generate base string with general info
+        message = "{0}, carrier={1}, container={2}: {3}."
+        message = message.format(str(self.__datetime), self.__carrier, self.__container, self.__message)
+        
+        # Generate custom message
+        if self.__exception is not None:
+            message += " " + str(self.__exception) #.replace("\n", "")
+        
+        return message
 
-try:
-    raise TrackingScraperError
-except TrackingScraperError as ex:
-    print(ex)
