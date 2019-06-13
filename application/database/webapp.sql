975cc3ea5ea213a2b636ac4e9d2b9a6ead018390
Agc96 <anthony.gutierrez@pucp.pe>
2019-06-13 01:02:12 -0500

    Avances al 2019-06-13

---
+++ application/database/webapp.sql
@@ -0,0 +1,2 @@
+-- TODO: Reemplazar 'pass' con la contraseña para la conexión a BD desde el sistema Web
+CREATE USER webapp WITH ENCRYPTED PASSWORD 'pass';
--- application/tracking/views.py
+++ application/tracking/views.py
@@ -2,5 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+"""
 def index(request):
-    return HttpResponse("Hola")
+    return HttpResponse("Login")
+"""
+
+def index(request):
+    return HttpResponse("Container List")
+
+def detail(request):
+    return HttpResponse("Container Detail")
+
+def create(request):
+    return HttpResponse("Container Create")
