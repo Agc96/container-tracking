eb977d0fb1e64ba8c69e358524001a8ca9d53767
Agc96 <anthony.gutierrez@pucp.pe>
2019-06-13 04:05:00 -0500

    Avances login y lista de contenedores 2019-06-13

--- application/tracking/urls.py
+++ application/tracking/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('new', views.new, name='new'),
-    path('<int:container_id>', views.detail, name='detail')
+    path('login/', views.login, name='login'),
+    path('containers/', views.container_list, name='container-list'),
+    path('containers/create/', views.container_create, name='container-create'),
+    path('containers/<int:container_id>/', views.container_detail, name='container-detail'),
+    path('profile/', views.profile, name='profile'),
+    path('logout/', views.logout, name='logout')
 ]
--- application/tracking/views.py
+++ application/tracking/views.py
@@ -1,17 +1,36 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.urls import reverse
 
 # Create your views here.
-"""
 def index(request):
-    return HttpResponse("Login")
-"""
+    return render(request, 'tracking/login.html', {
+        'logged': False
+    })
 
-def index(request):
-    return HttpResponse("Container List")
+def login(request):
+    return HttpResponseRedirect(reverse('container-list'))
 
-def detail(request):
-    return HttpResponse("Container Detail")
+def container_list(request):
+    return render(request, 'tracking/container/list.html', {
+        'logged': True,
+        'fullname': 'Anthony Gutiérrez',
+        'role': 'Administrador'
+    })
 
-def create(request):
+def container_detail(request, container_id):
+    return render(request, 'tracking/container/detail.html', {
+        'logged': True,
+        'fullname': 'Anthony Gutiérrez',
+        'role': 'Administrador',
+        'container_id': container_id
+    })
+
+def container_create(request):
     return HttpResponse("Container Create")
+
+def profile(request):
+    return HttpResponse('TODO: Perfil')
+
+def logout(request):
+    return HttpResponse('TODO: Logout')
