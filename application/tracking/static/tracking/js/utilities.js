be09d9d7aa18b11762b09a1fdd38343184a552ab
Agc96 <anthony.gutierrez@pucp.pe>
2019-06-13 19:32:21 -0500

    Avances 2019-06-13

--- application/tracking/static/tracking/js/utilities.js
+++ application/tracking/static/tracking/js/utilities.js
@@ -1,9 +1,7 @@
 jQuery(document).ready(function ($) {
 
 	/* Selects con placeholders */
-
 	$('select').each(selectPlaceholder).change(selectPlaceholder);
-
 	function selectPlaceholder() {
 		if (this.value === '') {
 			$(this).addClass('placeholder');
@@ -13,3 +11,29 @@ jQuery(document).ready(function ($) {
 	}
 
 });
+
+/* Métodos auxiliares */
+var Utilities = {
+	showMessage: function(messageType, messageContent, messageIcon, messageTitle) {
+		iziToast.show({
+			class: 'iziToast-' + (messageType || 'default'),
+			title: (messageTitle || ''),
+			message: (messageContent || ''),
+			animateInside: false,
+			position: 'topRight',
+			progressBar: false,
+			icon: (messageIcon || ''),
+			timeout: 3200,
+			transitionIn: 'fadeInLeft',
+			transitionOut: 'fadeOut',
+			transitionInMobile: 'fadeIn',
+			transitionOutMobile: 'fadeOut'
+		});
+	},
+	showErrorMessage: function(messageContent) {
+		Utilities.showMessage('danger', messageContent, 'icon-ban', 'Error');
+	},
+	showSuccessMessage: function(messageContent) {
+		Utilities.showMessage('success', messageContent, 'icon-check', 'Éxito');
+	}
+}
--- application/tracking/views.py
+++ application/tracking/views.py
@@ -2,35 +2,77 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-# Create your views here.
+from .models import Carrier
+
+# TODO: Reemplazar esto con una llamada a getenv()
+MAPS_KEY = 'KEY'
+
+# Vistas del sistema de información: Inicio de sesión
+
 def index(request):
+    """Formulario de inicio de sesión."""
     return render(request, 'tracking/login.html', {
-        'logged': False
+        'user': None
     })
 
 def login(request):
+    """Validación de datos y lógica de inicio de sesión."""
+    # TODO: Implementar lógica de inicio de sesión
     return HttpResponseRedirect(reverse('container-list'))
 
+# Vistas del sistema de información: Lista de contenedores
+
 def container_list(request):
+    """Búsqueda y listado de contenedores marítimos."""
     return render(request, 'tracking/container/list.html', {
-        'logged': True,
-        'fullname': 'Anthony Gutiérrez',
-        'role': 'Administrador'
+        'user': MockUser(),
+        'carriers': Carrier.objects.all()
     })
+
+def container_import(request):
+    # TODO: Implementar lógica para importar lista de contenedores
+    return HttpResponse('TODO: Importar lista de contenedores')
+
+def container_export(request):
+    # TODO: Implementar lógica para exportar lista de contenedores
+    return HttpResponse('TODO: Exportar lista de contenedores')
+
+# Vistas del sistema de información: Detalles del contenedor
 
 def container_detail(request, container_id):
+    """Detalles de un contenedor marítimo."""
     return render(request, 'tracking/container/detail.html', {
-        'logged': True,
-        'fullname': 'Anthony Gutiérrez',
-        'role': 'Administrador',
-        'container_id': container_id
+        'user': MockUser(),
+        'container_id': container_id,
+        'carriers': Carrier.objects.all()
     })
 
+# Vistas del sistema de información: Creación de contenedores
+
 def container_create(request):
-    return HttpResponse("Container Create")
+    """Formulario de creación de un contenedor marítimo."""
+    return render(request, 'tracking/container/create.html', {
+        'user': MockUser(),
+        'carriers': Carrier.objects.all(),
+        'maps_key': MAPS_KEY
+    })
+
+def container_save(request):
+    # TODO: Implementar lógica de guardado de contenedor
+    return HttpResponseRedirect(reverse('container-list'))
+
+# Vistas del sistema de información: Opciones de usuario
 
 def profile(request):
+    """Configuración del perfil de un usuario."""
     return HttpResponse('TODO: Perfil')
 
 def logout(request):
-    return HttpResponse('TODO: Logout')
+    # TODO: Implementar lógica de cierre de sesión
+    return HttpResponseRedirect(reverse('index'))
+
+# Funciones auxiliares
+
+class MockUser():
+    fullname = 'Anthony Gutiérrez'
+    role = 'Administrador'
