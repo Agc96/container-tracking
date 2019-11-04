bdceaa8829ad369b88160c97829e3d00a303b5ef
Agc96 <anthony.gutierrez@pucp.pe>
2019-11-04 17:30:56 -0500

    Cambiado mail server de SMTP+SSL a SMTP+TLS

--- scraper/final/mail.py
+++ scraper/final/mail.py
@@ -22,7 +22,7 @@ class ScraperEmail:
         "subject" : "{carrier} ha finalizado",
         "content" : "Se ha terminado la ejecución de la naviera {carrier} el {date}, con {counter} contenedores."
     }
-    
+
     def __init__(self, **kwargs):
         self.login_user   = os.getenv("MAIL_LOGIN_USER")
         self.login_pass   = os.getenv("MAIL_LOGIN_PASS")
@@ -31,7 +31,7 @@ class ScraperEmail:
         self.data         = kwargs
         self.data["user"] = self.send_name
         self.data["date"] = datetime.datetime.now().strftime("%d/%m/%Y a las %H:%M")
-    
+
     def send_message(self, content, subject):
         """Sends an email to the administrator."""
         # Parse message
@@ -41,10 +41,14 @@ class ScraperEmail:
         message = MIMEText(content, "plain", "UTF-8")
         message["Subject"] = subject.format(**self.data)
         # Send message through Gmail SMTP server
-        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+        with smtplib.SMTP("smtp.gmail.com", 587) as server:
+            server.ehlo()
+            server.starttls()
+            server.ehlo()
             server.login(self.login_user, self.login_pass)
             server.sendmail(self.login_user, self.send_user, message.as_string())
-    
+            server.quit()
+
     def send(self, msg_info):
         """Wrapper for `send_message`, passing a `dict` as message information."""
         self.send_message(msg_info["content"], msg_info["subject"])
